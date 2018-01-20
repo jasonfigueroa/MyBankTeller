f5095d172cd55402117c30ec8f7fd9d24ddebf33
Jason Figueroa <jason.figueroa2@gmail.com>
2018-01-20 17:16:44 -0600

    account creation during customer creation is now working

--- Model.cs
+++ Model.cs
@@ -58,7 +58,50 @@ namespace MyBankTeller
         {
             int accountId = 0;
 
-            
+            string connectionString = "Data Source=database.sqlite;";
+            SqliteConnection sqlite_conn = new SqliteConnection(connectionString);
+
+            sqlite_conn.Open();
+
+            SqliteCommand sqlite_command = sqlite_conn.CreateCommand();
+
+            /*
+            CREATE TABLE IF NOT EXISTS 
+                Account (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    customer_id INTEGER NOT NULL,
+                    balance REAL NOT NULL DEFAULT 0,
+					FOREIGN KEY (customer_id) REFERENCES Customer(id));
+					
+            insert into Account (customer_id, balance) values (1, 5.00);
+            */
+
+            sqlite_command.CommandText = @"CREATE TABLE IF NOT EXISTS 
+                Account (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    customer_id INTEGER NOT NULL,
+                    balance REAL NOT NULL DEFAULT 0,
+                    FOREIGN KEY (customer_id) REFERENCES Customer(id))"; 
+
+            sqlite_command.ExecuteNonQuery();
+
+            double initialBalance = 0;
+            sqlite_command.CommandText = $"INSERT INTO Account (customer_id, balance) VALUES ({customerId}, {initialBalance})";
+
+            sqlite_command.ExecuteNonQuery();
+
+            sqlite_command.CommandText = "SELECT LAST_INSERT_ROWID() as account_id";
+
+            SqliteDataReader dataReader = sqlite_command.ExecuteReader();
+
+            while(dataReader.Read())
+            {
+                // long id = (long) dataReader["id"];
+                // object id = dataReader["id"];
+                accountId = int.Parse(dataReader["account_id"].ToString());
+            }
+
+            sqlite_conn.Close();
 
             return accountId;
         }
